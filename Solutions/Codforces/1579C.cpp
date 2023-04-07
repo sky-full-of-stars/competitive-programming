27b3021ff1868312058d9baa67ad3e8aced07b94
asuryana: Akshay Suryanarayan <asuryana@akamai.com>
2023-04-07 15:05:02 +0530

    auto commit from local cron

---
+++ Solutions/Codforces/1579C.cpp
@@ -0,0 +1,156 @@
+#include "bits/stdc++.h"
+
+#ifndef ONLINE_JUDGE
+#include <bits/headerfile.hpp>
+#else
+#define dbg (...)
+#endif
+
+//---------------------------------------------------------------------------------------------------------//
+using namespace std;
+
+#define int long long int
+#define cont continue;
+#define ff first
+#define ss second
+#define pb emplace_back
+#define mp make_pair
+#define endl '\n'
+
+//---------------------------------------------------------------------------------------------------------//
+typedef vector<int> vi;
+typedef set<int> si;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef map<int, int> mi;
+typedef unordered_map<int, int> umi;
+
+//---------------------------------------------------------------------------------------------------------//
+#define EPS 1e-9
+#define INF LLONG_MAX
+const int mod = 1000 * 1000 * 1000 + 7; //1e9+7
+const double PI = 3.14159265358979323846264;
+const pi steps[] = {{1, 0}, {0, 1}, { -1, 0}, {0, -1}, {1, -1}, {1, 1}, { -1, -1}, { -1, 1}}; //for (auto [dx,dy] : steps)
+//const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1}, dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+
+//---------------------------------------------------------------------------------------------------------//
+template<typename... T>
+void read(T&... args) {
+	((cin >> args), ...);
+}
+
+template<typename... T>
+void write(T&&... args) {
+	((cout << args << " "), ...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) { cerr << *it << " = " << a << endl; err(++it, args...); }
+#else
+#define debug(...) 1
+#define cerr if(0) cerr
+#endif
+
+//---------------------------------------------------------------------------------------------------------//
+#define sz(a) (int)((a).size())
+#define present(c,x) ((c).find(x) != (c).end())
+#define ipArr(a,n)   for(int i=0;i<n;i++) cin>>a[i];
+#define opArr(arr,n) for(int i=0;i<n;i++) cout<<arr[i]<<"\n"; cout<<endl;
+#define fill_arr(arr,n,i) fill(arr,arr+n,i)
+#define fill_vec(v,n,i) fill(v.begin(),v.begin()+n,i);
+#define sortv(a) sort(a.begin(),a.end())
+#define all(i) i.begin(),i.end()
+
+int gcd(int a, int b) {return b ? gcd (b, a % b) : a;}
+bool isPowOfTwo(int x) {return (x && (!(x & (x - 1))));}
+//---------------------------------------------------------------------------------------------------------//
+
+
+const int maxN = 1e7;
+
+int n, m, k;
+char v[100][100];
+
+bool valid(pair<int, int> p)
+{
+	int i = p.ff;
+	int j = p.ss;
+	return i >= 0 and i<n and j >= 0 and j < m and v[i][j] == '*';
+}
+
+void solve()
+{
+	cin >> n >> m >> k;
+
+	bool vis[n][m];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> v[i][j];
+			vis[i][j] = false;
+		}
+	}
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = m - 1; j >= 0; j--)
+		{
+			if (v[i][j] == '*')
+			{
+				int sz = 0;
+				pi left = {i - 1, j - 1};
+				pi right = {i - 1, j + 1};
+
+				while (valid(left) and valid(right))
+				{
+					sz++;
+					left = {left.ff - 1, left.ss - 1};
+					right = {right.ff - 1, right.ss + 1};
+				}
+
+				if (sz >= k)
+				{
+					pi left = {i - 1, j - 1};
+					pi right = {i - 1, j + 1};
+					while (valid(left) and valid(right))
+					{
+						vis[left.ff][left.ss] = true;
+						vis[right.ff][right.ss] = true;
+
+						left = {left.ff - 1, left.ss - 1};
+						right = {right.ff - 1, right.ss + 1};
+					}
+				}
+				else if (!vis[i][j])
+				{
+					cout << "No" << endl;
+					return;
+				}
+			}
+		}
+	}
+
+	cout << "Yes" << endl;
+
+}
+
+void setUpLocal()
+{
+#ifndef ONLINE_JUDGE
+	freopen("/Users/asuryana/Documents/CP/input.txt", "r", stdin);
+	freopen("/Users/asuryana/Documents/CP/output.txt", "w", stdout);
+#endif
+}
+
+int32_t main()
+{
+	cin.tie(nullptr)->sync_with_stdio(false);
+	setUpLocal();
+	int t = 1; cin >> t;
+	while (t--) solve();
+	return 0;
+}
