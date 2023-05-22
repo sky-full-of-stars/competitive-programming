98c0df0f9b1660b94772979c762bcf5c983ad1a9
asuryana: Akshay Suryanarayan <asuryana@akamai.com>
2023-05-22 15:05:00 +0530

    auto commit from local cron

---
+++ Solutions/CSES/Dynamic programming/1093.cpp
@@ -0,0 +1,123 @@
+#include "bits/stdc++.h"
+
+#ifndef ONLINE_JUDGE
+#include <bits/headerfile.hpp>
+#else
+#define dbg (...)
+#endif
+
+//---------------------------------------------------------------------------------------------------------//
+using namespace std;
+
+#define cont continue;
+#define ff first
+#define ss second
+#define pb emplace_back
+#define mp make_pair
+#define endl '\n'
+
+//---------------------------------------------------------------------------------------------------------//
+typedef vector<int> vi;
+typedef set<int> si;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+typedef map<int, int> mi;
+typedef unordered_map<int, int> umi;
+
+//---------------------------------------------------------------------------------------------------------//
+#define EPS 1e-9
+#define INF LLONG_MAX
+const int mod = 1000 * 1000 * 1000 + 7; //1e9+7
+const double PI = 3.14159265358979323846264;
+const pi steps[] = {{1, 0}, {0, 1}, { -1, 0}, {0, -1}, {1, -1}, {1, 1}, { -1, -1}, { -1, 1}}; //for (auto [dx,dy] : steps)
+//const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1}, dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+
+//---------------------------------------------------------------------------------------------------------//
+template<typename... T>
+void read(T&... args) {
+	((cin >> args), ...);
+}
+
+template<typename... T>
+void write(T&&... args) {
+	((cout << args << " "), ...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) { cerr << *it << " = " << a << endl; err(++it, args...); }
+#else
+#define debug(...) 1
+#define cerr if(0) cerr
+#endif
+
+//---------------------------------------------------------------------------------------------------------//
+#define sz(a) (int)((a).size())
+#define present(c,x) ((c).find(x) != (c).end())
+#define ipArr(a,n)   for(int i=0;i<n;i++) cin>>a[i];
+#define opArr(arr,n) for(int i=0;i<n;i++) cout<<arr[i]<<"\n"; cout<<endl;
+#define fill_arr(arr,n,i) fill(arr,arr+n,i)
+#define fill_vec(v,n,i) fill(v.begin(),v.begin()+n,i);
+#define sortv(a) sort(a.begin(),a.end())
+#define all(i) i.begin(),i.end()
+
+int gcd(int a, int b) {return b ? gcd (b, a % b) : a;}
+bool isPowOfTwo(int x) {return (x && (!(x & (x - 1))));}
+//---------------------------------------------------------------------------------------------------------//
+
+
+const int maxN = 1e7;
+
+void solve()
+{
+	int n; cin >> n;
+
+	if ((n * (n + 1) / 2) & 1)
+	{
+		cout << "0";
+		return;
+	}
+
+	int x = (n * (n + 1)) / 4;
+	//long long dp[n + 1][x + 1];
+	vector<vector<long>> dp(n + 1, vector<long>(x + 1));
+	//memset(dp, 0, sizeof dp);
+	for (int i = 0; i <= x ; i++)
+	{
+		if (i == 0)
+			dp[0][i] = 1;
+		else
+			dp[0][i] = 0;
+	}
+
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int sum = 0; sum <= x; sum++)
+		{
+			int pick = (sum - i >= 0) ? dp[i - 1][sum - i] : 0;
+			int nonPick = dp[i - 1][sum];
+
+			dp[i][sum] = (pick + nonPick) % mod;
+		}
+	}
+
+	cout << (dp[n][x] * 500000004) % mod;
+}
+void setUpLocal()
+{
+#ifndef ONLINE_JUDGE
+	freopen("/Users/asuryana/Documents/CP/input.txt", "r", stdin);
+	freopen("/Users/asuryana/Documents/CP/output.txt", "w", stdout);
+#endif
+}
+int32_t main()
+{
+	cin.tie(nullptr)->sync_with_stdio(false);
+	setUpLocal();
+	int t = 1; //cin>>t;
+	while (t--) solve();
+	return 0;
+}
